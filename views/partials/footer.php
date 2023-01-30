7ade2d70c1d8974d1b4ac965efde6f9bb1214b2e
sophorntoeum <sophorn.toeum@student.passerellesnumeriques.org>
2023-01-30 20:33:52 +0700

    create footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,61 +1,64 @@
-<footer class="bg-primary text-white text-center text-lg-start">
+
+<?php 
+require "views/partials/header.php"
+?>
+<footer class="bg-secondary text-white text-center text-lg-start">
   <!-- Grid container -->
   <div class="container p-4">
     <!--Grid row-->
     <div class="row">
       <!--Grid column-->
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Footer Content</h5>
-
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
-          voluptatem veniam, est atque cumque eum delectus sint!
-        </p>
-      </div>
-      <!--Grid column-->
-
-      <!--Grid column-->
       <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Links</h5>
+        <h5 class="text-uppercase">MOVIE</h5>
 
-        <ul class="list-unstyled mb-0">
+        <ul class="list-unstyled mb0">
           <li>
-            <a href="#!" class="text-white">Link 1</a>
+            <a href="#!" class="text-white">NOW SHOWING</a>
           </li>
           <li>
-            <a href="#!" class="text-white">Link 2</a>
+            <a href="#!" class="text-white">COMING SOON</a>
           </li>
           <li>
-            <a href="#!" class="text-white">Link 3</a>
+            <a href="#!" class="text-white">CINEMAS</a>
           </li>
           <li>
-            <a href="#!" class="text-white">Link 4</a>
+            <a href="#!" class="text-white">NEWS & ACTIVITY</a>
           </li>
         </ul>
       </div>
       <!--Grid column-->
-
+      
       <!--Grid column-->
       <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 class="text-uppercase mb-0">Links</h5>
+        <h5 class="text-uppercase mb-0">SYSTEM TYPE</h5>
 
         <ul class="list-unstyled">
           <li>
-            <a href="#!" class="text-white">Link 1</a>
+            <a href="#!" class="text-white">4DX</a>
           </li>
           <li>
-            <a href="#!" class="text-white">Link 2</a>
+            <a href="#!" class="text-white">IMAX</a>
           </li>
           <li>
-            <a href="#!" class="text-white">Link 3</a>
+            <a href="#!" class="text-white">KIDS CINEMA</a>
           </li>
           <li>
-            <a href="#!" class="text-white">Link 4</a>
+            <a href="#!" class="text-white">SCREEN X</a>
           </li>
         </ul>
       </div>
       <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">MOVIE LINE</h5>
+        <h1>097-73-48-624</h1>
+
+        <i class="fa fa-facebook-official" aria-hidden="true"></i>
+        <i class="fa fa-facebook-official" aria-hidden="true"></i>
+      </div>
+      <!--Grid column-->
+
     </div>
     <!--Grid row-->
   </div>
@@ -63,7 +66,6 @@
 
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2020 Copyright:
     <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
   </div>
   <!-- Copyright -->
