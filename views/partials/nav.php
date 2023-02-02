e1a7e6420499d66277ca48e996b3221162249225
SocheatLun <socheat.lun@student.passerellesnumeriques.org>
2023-02-02 13:58:12 +0700

    review code

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -44,6 +44,11 @@
       <!-- Left links -->
     </div>
     </div>
+    <form action="search.php" method="post" class="d-flex flex-row m-3 gap-2" style="width:50%">
+      <input type="text" name="search" class="form-control mt-2 mr-7"
+      placeholder="Type here to search....">
+      <button type="submit" name="submit" class="btn btn-success mt-2 mr-7 bg-warning border-warning w-25">search</button>
+    </form>
     <?php
   
     if(!isset($_SESSION["email"]))
