c9d68ae9e8d6bdccae3ea4b25475da7b762847d7
sophorntoeum <sophorn.toeum@student.passerellesnumeriques.org>
2023-02-07 16:08:31 +0700

    [MOV-46]:

---
+++ admin/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>seller Admin</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <form>
+        <h2>LOGIN</h2>
+        <label>User Name</label>
+        <input type="text" name="uname"placeholder ="User Name"><br>
+
+        <label>User Password</label>
+        <input type="password" name="password" placeholder ="password"><br>
+
+        <button type ="submit">Login</button>
+    </form>
+</body>
