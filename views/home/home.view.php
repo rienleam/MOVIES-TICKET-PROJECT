e1775fe6d241220f43ca58ea614a836f88f0dc89
rienleam <rienleam@gmail.com>
2023-02-08 15:50:00 +0700

    [MOV-37] :As a Customer, I want to view details of a show

--- views/home/home.view.php
+++ views/home/home.view.php
@@ -41,15 +41,15 @@
         <img src="<?php echo $item['image']; ?>" class="card-img-top" style = "width: 100%; height: 270px" alt="Fissure in Sandstone"/>
         <div class="card-body">
             <h4 class="card-title fw-bold"><?php echo $item['show_name']; ?></h4>
-            <div class="date">
+            <div class="date m-0">
                 <i class="large material-icons">date_range</i>
                 <p class="card-text"><?php echo $item['date']; ?></p>
             </div>
-            <div class="time">
+            <div class="time m-0">
                 <i class="large material-icons">alarm_on</i>
                 <p class="card-text"><?php echo $item['time']; ?></p>
             </div>
-            <div class="duration">
+            <div class="duration m-0">
                 <i class="large material-icons">access_alarms</i>
                 <p class="card-text"><?php echo $item['duration']; ?></p>
             </div>
