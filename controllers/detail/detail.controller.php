85221b7004c2f2154069bf239dd17e1f8de68b73
rienleam <rienleam@gmail.com>
2023-02-08 14:04:59 +0700

    [MOV-37] As a Customer, I want to view details of a show

--- controllers/detail/detail.controller.php
+++ controllers/detail/detail.controller.php
@@ -14,14 +14,14 @@ $getID = ($_GET['show_id']);
 $movies = getDetailMovies($getID);
 ?>
 
-<div class="card m-auto mt-50" style="width: 90%; height: 80%;">
+<div class="card m-auto mt-5 p-4" style="width: 90%; height: 80%;">
   <div class="row g-0">
     <div class="col-md-4">
       <img
         src="<?=$movies['image'];?>"
         alt="Trendy Pants and Shoes"
         class="img-fluid rounded-start"
-        style="width: 120%; height: 138%;"
+        style="width: 350px; height: 350px;"
       />
     </div>
     <div class="col-md-7">
@@ -30,8 +30,21 @@ $movies = getDetailMovies($getID);
         <p class="card-text">
         <?=$movies['description'];?>
         </p>
-        <p class="card-text">
+        <p class="card-text d-flex align-items-center">
+        <i class="large material-icons">business_center</i> 
+          <small class="text-muted">$<?=$movies['cost'];?></small>
+        </p>
+        <p class="card-text d-flex align-items-center">
+          <i class="large material-icons">date_range</i>
           <small class="text-muted"><?=$movies['date'];?></small>
+        </p>
+        <p class="card-text d-flex align-items-center">
+          <i class="large material-icons">alarm_on</i>
+          <small class="text-muted"><?=$movies['time'];?></small>
+        </p>
+        <p class="card-text d-flex align-items-center">
+          <i class="large material-icons">access_alarms</i>
+          <small class="text-muted"><?=$movies['duration'];?></small>
         </p>
       </div>
     </div>
