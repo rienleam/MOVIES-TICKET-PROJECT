66313c33565880d83580389b9802b4bdd19ab1fd
rienleam <rienleam@gmail.com>
2023-02-10 10:57:35 +0700

    [MOV-76]: Create Contact us and About us page

--- controllers/contact/contact.controller.php
+++ controllers/contact/contact.controller.php
@@ -2,6 +2,68 @@
 require "views/partials/header.php";
 require "views/partials/nav.php";
 
+function validate_username(string $username) : bool
+{
+   return !ctype_alnum( $username);
+}
+
+function validate_email(string $e_mail) : bool
+{
+   return !filter_var( $e_mail,FILTER_VALIDATE_EMAIL);
+}
+
+$username_error = '';
+$e_mail_error = '';
+$telephone_error = '';
+$topic_error = '';
+$message_error = ''; 
+
+$task_complete = 0;
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'] ? $_POST['username'] : '';
+    $e_mail = $_POST['e-mail'] ? $_POST['e-mail'] : '';
+    $telephone = $_POST['telephone'] ? $_POST['telephone'] : '';
+    $topic = $_POST['the-topic'] ? $_POST['the-topic'] : '';
+    $message = $_POST['user-message'] ? $_POST['user-message'] : '';
+
+    if (empty($username)) {
+        $username_error = "Name required";
+    } else if (!empty($username)){
+        $task_complete += 1;
+    }
+    if (empty($e_mail)) {
+        $e_mail_error = "Email required";
+    } else if(!empty($e_mail)){
+        $email_validate = validate_email($e_mail);
+        if ($email_validate) {
+            $e_mail_error = "Email required '@";
+        } else {
+            $task_complete += 1;
+        }
+    }
+    if (empty($telephone)) {
+        $telephone_error = "Phone number required";
+    } else if (!empty($telephone)) {
+        $task_complete += 1;
+    }
+    if (empty($topic)) {
+        $topic_error = "Topic required";
+    } else if (!empty($topic)) {
+        $task_complete += 1;
+    }
+    if (empty($message)) {
+        $message_error = "Message required";
+    } else if (!empty($message)) {
+        $task_complete += 1;
+    }
+    if ($task_complete == 5) {
+        inputDataInToDB($username, $telephone, $e_mail, $topic, $message);
+        header("Location: /");
+    }
+}
+
 require "views/contact/contact.view.php";
 
 require "views/partials/footer.php";
--- views/contact/contact.view.php
+++ views/contact/contact.view.php
@@ -1,25 +1,70 @@
 <div class="contact">
-    <h1>Contact Us</h1>
     <div class="message">
         <div class="position">
-            <h3>Business Development</h3>
-            <p>Working hour : Morning (7:30AM - 11:30AM) Afternoon (1:00PM - 5:00PM)</p>
-            <p>Telephone : 097 73 48 624</p>
-            <p>Email : rienleam@gmail.com</p>
-            <h3>Marketing</h3>
-            <p>Working hour : Morning (7:30AM - 11:30AM) Afternoon (1:00PM - 5:00PM)</p>
-            <p>Telephone : 097 73 48 624</p>
-            <p>Email : rienleam@gmail.com</p>
+            <h3 class="mb-4"><b class="text-warning">Business Development</b></h3>
+            <p><b>WORKING HOUR :</b> <br>Morning (7:30AM - 11:30AM) Afternoon (1:00PM - 5:00PM)</p>
+            <p><b>TELEPHONE :</b> 097 73 48 624</p>
+            <p><b>EMAIL :</b> rienleam@gmail.com</p>
+            <h3 class="mb-4"><b class="text-warning">Marketing</b></h3>
+            <p><b>WORKING HOUR :</b> <br>Morning (7:30AM - 11:30AM) Afternoon (1:00PM - 5:00PM)</p>
+            <p><b>TELEPHONE :</b> 097 73 48 624</p>
+            <p><b>EMAIL :</b> rienleam@gmail.com</p>
         </div>
         <hr>
         <div class="form-contact">
+            <h2 class="text-warning mb-4"><b>Contact Us</b></h2>
             <form class="chat">
-                <div class="username"></div>
-                <div class="e-mail"></div>
-
-                <label class="mt-1" for="password">Password</label>
-                <input type="password" name="password" placeholder="Enter your password">
-                <small class="form-text text-danger"> <?php echo $password_error; ?></small>
+                <div class="username mb-3">
+                    <div class="label">
+                        <label class="mt-1" for="username">Your name</label>
+                    </div>
+                    <div class="input">
+                        <input type="text" name="username" placeholder="Enter your name" style="width: 100%; padding: 5px;">
+                        <small class="form-text text-danger"> <?php echo $username_error; ?></small> 
+                    </div>
+                </div>
+                <div class="e-mail mb-3">
+                    <div class="label">
+                        <label class="mt-1" for="e-mail">Email</label>
+                    </div>
+                    <div class="input">
+                        <input type="email" name="e-mail" placeholder="Enter your email" style="width: 100%; padding: 5px;">
+                        <small class="form-text text-danger"> <?php echo $e_mail_error; ?></small> 
+                    </div>
+                </div>
+                <div class="telephone mb-3">
+                    <div class="label">
+                        <label class="mt-1" for="telephone">Telephone</label>
+                    </div>
+                    <div class="input">
+                        <input type="number" name="telephone" placeholder="Enter your telephone" style="width: 100%; padding: 5px;">
+                        <small class="form-text text-danger"> <?php echo $telephone_error; ?></small> 
+                    </div>
+                </div>
+                <div class="topic mb-3">
+                    <div class="label">
+                        <label class="mt-1" for="topic">Topic</label>
+                    </div>
+                    <div class="input">
+                        <select name="the-topic" id="topic" style="width: 100%; padding: 7px;">
+                            <option value="nothing" selected disabled>Please select a topic</option>
+                            <option value="comment">Comment and Feedback</option>
+                            <option value="apply">Apply for a job</option>
+                            <option value="partner">Co-partner</option>
+                        </select>
+                        <small class="form-text text-danger"> <?php echo $topic_error; ?></small> 
+                    </div>
+                </div>
+                <div class="chat-message mb-3">
+                    <div class="label">
+                        <label class="mt-1" for="user-message">Message</label>
+                    </div>
+                    <div class="input">
+                        <textarea name="user-message" id="user-message" cols="5" rows="4" placeholder="Enter your message" style="width: 100%;"></textarea>
+                        <small class="form-text text-danger"><?php echo $message_error; ?></small> 
+                    </div>                  
+                </div>
+                <button type="submit" name="submit" class="send">SEND</button>
             </form>
         </div>        
     </div>
