f45a3c8ada908df61a3db0987d911708e09cb6cb
rienleam <rienleam@gmail.com>
2023-02-16 09:58:41 +0700

    [MOV-70]: Create form for booikng

--- views/post/post.booking.view.php
+++ views/post/post.booking.view.php
@@ -1,227 +1,52 @@
-<?php
+<?php 
 require '../partials/header.php';
+require "../../controllers/booking/booking.controller.php";
 ?>
-
-
-<form class="form d-flex m-auto mt-5 flex-column text-light">
-    <h1 class="text-center m-2">Choose Seats</h1>
-    <div class="status_seats1 m-2">
-        <h4>Seats: </h4>
-        <label class="checkbox" for="myCheckboxId">
-        <i class="fa fa-stop fa-sold bg-info text-info" aria-hidden="true"></i><span class="sold">Sold</span> 
-        </label>
-    </div>
-    <div class="status_seats2 m-2">
-        <h4>Price: </h4>
-        <label class="checkbox" for="myCheckboxId">
-        <i class="fa fa-stop fa-selected bg-warning text-warning" aria-hidden="true"></i><span class="select">Selected</span> 
-        </label>
-    </div>
-    <div class="status_seats3 m-2">
-        <h4>Time: </h4>
-        <label class="checkbox" for="myCheckboxId">
-        <i class="fa fa-stop fa-available bg-danger text-danger" aria-hidden="true"></i><span class="available">Available</span> 
-        </label>
-    </div>
-    <div class="checkboxs m-3">
-        <label class="checkbox" for="myCheckboxId">1
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-        </label>
-        <label class="checkbox" for="myCheckboxId">2
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-        </label>
-        <label class="checkbox" for="myCheckboxId">3
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-        </label>
-        <label class="checkbox" for="myCheckboxId">4
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-        </label>
-        <label class="checkbox" for="myCheckboxId">5
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-        </label>
-        <label class="checkbox" for="myCheckboxId">6
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-        </label>
-        <label class="checkbox" for="myCheckboxId">7
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-        </label>
-        <label class="checkbox" for="myCheckboxId">8
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2">
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-            <input type="checkbox" class="ckeckbox__input m-2" id="myCheckboxId">
-            <input type="checkbox" class="ckeckbox__box m-2"><br>
-        </label>
-        
-    </div>
-
-    <button type="submit" class="btn btn-default bg-warning">Booking Now</button>
-</form>
-
-
-
-
+<div class="payment d-flex justify-content-start align-items-start" style="width: 100%;">
+    <form action="<?php $action ?>" method="post" class="buy-info bg-white d-flex flex-column p-5" style="width: 40%; height:100%">
+        <h2 class="text-warning align-self-center mb-3"><b>Movie Ticket Booking</b></h2>
+        <div class="movie-title d-flex justify-content-between">
+            <p class="p-0 m-0">Theater name:</p>
+            <p class="p-0 m-0"><b>Monster Hunter</b></p>
+        </div>
+        <div class="movie-date d-flex justify-content-between">
+            <p class="p-0 m-0">Show date:</p>
+            <p class="p-0 m-0"><b>2023-02-27</b> </p>
+        </div>
+        <div class="movie-time d-flex justify-content-between">
+            <p class="p-0 m-0">Show time:</p>
+            <p class="p-0 m-0"><b>8:00AM - 11:30AM</b></p>
+        </div>
+        <h3 class="align-self-start"><b>Cost</b></h3>
+        <div class="seat-price d-flex justify-content-between">
+            <p class="p-0 m-0">Price/seat:</p>
+            <p class="p-0 m-0">$30</p>
+        </div>
+        <div class="seat-number d-flex justify-content-between">
+            <p class="p-0 m-0">Number of tickets:</p>
+            <input type="number" name="ticket-number" style="width:20%;">
+        </div>
+        <small class="form-text text-danger align-self-end"><?php echo $number_ticket_error; ?></small>
+        <hr class="p-0 m-0 mt-2 mb-2">
+        <div class="total-price d-flex justify-content-between">
+            <p class="p-0 m-0"><b>Total Price:</b></p>
+            <p class="p-0 m-0"><b>$60</b></p>
+        </div>
+        <h3 class="align-self-start"><b>Your Payment</b></h3>
+        <div class="payment-select d-flex justify-content-between">
+            <p class="p-0 m-0">Choose payment:</p>
+            <select name="choose-payment" id="choose-payment" class="p-2" style="width: 60%;">
+                <option value="none" selected disabled>---------choose your card---------</option>
+                <option value="aba">ABA</option>
+                <option value="wing">WING</option>
+                <option value="vattanac">VATTANAC</option>
+                <option value="acleda">ACLEDA</option>
+            </select>
+        </div>
+        <small class="form-text text-danger align-self-end"> <?php echo $payment_error; ?></small>
+        <div class="user-pay d-flex justify-content-between mt-4">
+            <button type="submit" name="submit" class="cancel-booking bg-warning">Cancel</button>
+            <button type="submit" name="submit" class="pay-now bg-warning">Pay now</button>
+        </div>
+    </form>
+</div>
--- views/register/register.view.php
+++ views/register/register.view.php
@@ -1,7 +1,7 @@
 <?php
 
  require 'views/partials/header.php';
-//  require 'views/partials/nav.php';
+
 ?>
 <link rel="stylesheet" href="views/css/style.register.view.css">
 <form action= "<?php $action ?>"  method="post">
@@ -40,4 +40,4 @@
     </form>
 
 <?php
-    //require 'views/partials/footer.php';
+    
